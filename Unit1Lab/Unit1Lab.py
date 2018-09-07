18978b1ff98b9f4caffda70395af7b89fd5774bb
IsaacBP2020 <20VaughnIsaac@bprep.org>
2018-09-07 09:17:38 -0700

    Unit1 Lab

---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,19 @@
+#print ("I love python")
+#myName = input ("What's your name?")
+
+#if myName : "Nothing" "Not important" "Skip" "John Smith"
+#print ('Fine, don\'t tell me')
+
+#print (input (2*2))
+#print (input (2**3))
+
+#num1=input ()
+#num2=input ()
+#print (num1+num2)
+
+#print (int(num1)+int(num2))
+
+intA=25
+intB=25
+intC=25
+print (intA == intB)
