ca1a3f812bf3b8a3bdc316165c78d6d3c501a0a9
Dunkleosteus77 <19iyengarnikhil@bprep.org>
2018-11-19 09:18:01 -0800

    color

--- TurtleLab/TurtleLab.py
+++ TurtleLab/TurtleLab.py
@@ -68,43 +68,51 @@ def circle():
         pen.left(120)
 #circle()
 
+def main():
+    count=int(input("How many shapes do you want? "))
+    for x in range(count):
+        advanced()
+
 def advanced():
-    color=input("Do you want green, blue, red, purple, or yellow ")
-    if color == "green" or "blue" or "red" or "purple" or "yellow":
+    color=input("Do you want green, blue, red, purple, or yellow? ")
+    if color == "green" or color == "blue" or color=="red" or color=="purple" or color=="yellow":
         pen.color(color)
-    if color != "green" or "blue" or "red" or "purple" or "yellow":
+    elif color != "green" or color != "blue" or color != "red" or color != "purple" or color != "yellow":
         print("I don't know what that is, so I picked red for you")
         pen.color("red")
     sides=int(input("How many sides do you want? "))
+    if sides == 2 or sides == 1 or sides < 0:
+        print("We make shapes here. I made you a triangle so you can get the gist here")
+        sides=3
     length=int(input("How long is each side? "))
+    if length > 500:
+        print("That's a very big number that I can't be bothered to deal with, so enjoy that at 200 px")
+        length=200
+    if length < 50:
+        print("Have you considered that length's too small? I drew it at 200 px instead")
+        length=200
+    x=int(input("What's your starting point's x-coordinate? "))
+    if x > 400 or x < -400:
+        print("That value is too dumb, I set it to 0")
+        x=0
+    y=int(input("What's the y coordinate? "))
+    if y > 400 or y < -400:
+        print("That value is too dumb, I set it to 0")
+        y=0
+    print("Your shape's done")
+    pen.up()
+    pen.setpos(x,y)
+    pen.down()
     if sides > 2:
         angle=(180*(sides-2))/sides
+    if sides==0:
+        pen.circle(length)
     if sides > 2 and length <= 500 and length >= 50:
         for x in range(sides):
             pen.forward(length)
             pen.left(180-angle)
-    elif sides <= 2 and length > 50 and length <= 500:
-        print("We make shapes here. I made you a triangle so you can get the gist here")
-        for x in range(3):
-            pen.forward(length)
-            pen.left(120)
-    elif length > 500 and sides > 2:
-        print("That's a very big number that I can't be bothered to deal with, so enjoy that at 200 px")
-        for x in range(sides):
-            pen.forward(200)
-            pen.left(180-angle)
-    elif length < 50 and sides > 2:
-        print("Have you considered that length's too small? I drew it at 200 px instead")
-        for x in range(sides):
-            pen.forward(200)
-            pen.left(180-angle)
-    elif sides <=2 and length < 50 or sides <=2 and length > 500:
-        print("You have broken the system. You will get a red triangle and you will like it")
-        pen.color("red")
-        for x in range(3):
-            pen.forward(200)
-            pen.left(120)
-advanced()
+main()
 
 turtle.update()
 turtle.exitonclick()
+
